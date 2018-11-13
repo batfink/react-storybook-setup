23d8e613d1b99d758b54b1a32dcfa432a074ca9e
Victor Nystad <vnystad@gmail.com>
2018-11-13 11:17:26 +0100

    chore(foobar): add dependency

--- packages/atoms/Foobar/Foobar.jsx
+++ packages/atoms/Foobar/Foobar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import Barbar from 'batfink-barbar'
+import PropTypes from 'prop-types'
 
 const Foobar = styled.div`
     display: grid;
@@ -9,6 +11,20 @@ const Foobar = styled.div`
     background: deeppink;
     color: lime;
     border: 1px solid black;
+    padding: 1rem;
 `
 
-export default Foobar
+const Component = ({ children }) => (
+    <Foobar>
+        <Barbar>{ children }</Barbar>
+    </Foobar>
+)
+
+Component.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.element,
+        PropTypes.arrayOf(PropTypes.element),
+    ]).isRequired,
+}
+
+export default Component
