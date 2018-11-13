b60fc3240a019fef7a9051ede83e4571a41eabca
Victor Nystad <vnystad@gmail.com>
2018-11-13 08:06:26 +0100

    update

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+const config = {
+    rules: {
+        'type-empty': [
+            2,
+            'never',
+        ],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'foobar',
+            ],
+        ],
+    },
+}
