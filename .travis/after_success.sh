267c2e53e21ffe65cf71dad9fc0f1cfa890dcd4a
Victor Nystad <vnystad@gmail.com>
2018-11-20 10:39:45 +0100

    chore(travis): update config

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -7,5 +7,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 fi
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
+    echo "We are in master, and we're about to run semantic release"
     npm run semantic-release
 fi
